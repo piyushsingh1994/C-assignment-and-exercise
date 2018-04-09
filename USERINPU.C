f146ba2060663b18cb3767d7872b1df0b389656a
Piyush Singh <piyushkumar194@gmail.com>
2018-04-09 15:13:29 +0530

    multiple of two number

---
+++ USERINPU.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+int x,y;
+clrscr();
+printf("enter first and second number");
+scanf("%d%d",&x,&y);
+printf("multiplicaton of %d and %d is %d",x,y,x*y);
+getch();
+}
+
