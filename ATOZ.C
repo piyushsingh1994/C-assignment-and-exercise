312696605ad86810b53c78ca8c7ad8f6400c60c9
Piyush Singh <piyushkumar194@gmail.com>
2018-04-13 15:57:46 +0530

    To print all alphabets using while loop

---
+++ ATOZ.C
@@ -0,0 +1,16 @@
+/** Program to print all alphabet using while loop **/
+
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+char ch='a';
+clrscr();
+while(ch<='z')
+{
+printf("%c\n",ch);
+ch++;
+}
+getch();
+
+}
