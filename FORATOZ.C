fcaf03c3e8d183ee3ef0c1ec8e9b63b6392af474
Piyush Singh <piyushkumar194@gmail.com>
2018-04-13 15:58:11 +0530

    To print all alphabets using for loop

---
+++ FORATOZ.C
@@ -0,0 +1,15 @@
+/** Program to print all alphabet using for loop **/
+
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+char ch='a';
+clrscr();
+for(ch='a';ch<='z';ch++)
+{
+printf("%c\n",ch);
+}
+getch();
+
+}
